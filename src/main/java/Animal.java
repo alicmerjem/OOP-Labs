d14d8b9ee0162ee44dcb23bb4278cac96f4b13ce
Merjem Alić <merjem.alic@stu.ibu.edu.ba>
2025-04-20 23:30:02 +0200

    Animal.java

---
+++ src/main/java/Animal.java
@@ -0,0 +1,19 @@
+public abstract class Animal {
+  private String name;
+  private int age;
+
+  public Animal(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public abstract void speak(); // abstract methods don't have a body
+}
