309d0f38e0003c6ec118ef0aff479c34953afde9
Merjem Alić <merjem.alic@stu.ibu.edu.ba>
2025-04-20 23:34:51 +0200

    Elephant.java

---
+++ src/main/java/Elephant.java
@@ -0,0 +1,15 @@
+public class Elephant extends Animal implements Comparable<Elephant> {
+  public Elephant(String name, int age) {
+    super(name, age);
+  }
+
+  @Override 
+  public void speak() {
+    System.out.println(name + " the elephant says: Trumpet!");
+  }
+
+  @Override 
+  public int compareTo(Elephant other) {
+    return Integer.compare(this.age, other.age);
+  }
+}
